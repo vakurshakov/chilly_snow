92c8075a6aa6b4190b71ef0306efa6f51cbd2e56
vladislav <crimsonschist@gmail.com>
2022-06-07 20:25:58 +0700

    Add game score

---
+++ include/score.h
@@ -0,0 +1,28 @@
+#ifndef SCORE_H
+#define SCORE_H
+
+/**
+ * @file    score.h
+ * @author  Vladislav Kurshakov
+ * @brief   Score calculation and drawing.
+ * @version 0.1
+ * @date    2022-05-30
+ * 
+ */
+
+#include <stdio.h>  // for sprintf
+
+#include "text.h"
+#include "ball.h"   // score calculated from ball_speed
+
+
+//! @note Uses initial ball_speed and BALL_SPEED_ADD 
+char*
+calculate_score();
+
+
+void
+draw_score();
+
+
+#endif // SCORE_H
---
+++ src/score.c
@@ -0,0 +1,35 @@
+#include "score.h"
+
+
+static text_t score = {
+    .y = -70,
+    .r = 0.447f,
+    .g = 0.501f,
+    .b = 1.0f,
+};
+
+
+char*
+calculate_score()
+{
+    extern float ball_speed;
+    int score = (int) ((ball_speed - 0.09f) / (10.0f * BALL_SPEED_ADD));
+
+    static char score_string[500];
+    sprintf(score_string, "%d", score);
+
+    return score_string;
+}
+
+
+void
+draw_score()
+{
+    score.text = calculate_score();
+
+    float width = stb_easy_font_width(score.text);
+
+    score.x = - (float) width / 2.; 
+
+    draw_text(score);
+}
